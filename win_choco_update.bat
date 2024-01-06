8be8cf1a06c26db8c5388448666e4f34bd4a1f1a
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-06 09:25:28 -0800

    made a few updates to the batch script

--- win_choco_update.bat
+++ win_choco_update.bat
@@ -1,17 +1,18 @@
-REM This is a PS script that installs all available Chocolatey and winget updates,
-REM then it starts the Windows Update service and opens up the GUI for it;
-REM PLEASE NOTE YOU MUST RUN POWERSHELL AS ADMINISTRATOR!
+REM - This is a batch script that installs all available Chocolatey and winget updates,
+REM  - then it starts the Windows Update service and opens up the GUI for it;
+REM - PLEASE NOTE YOU MUST RUN CMD AS ADMINISTRATOR!
 
-REM Install all available Chocolatey updates;
+REM - Install all available Chocolatey updates;
 choco upgrade all
 
-REM Install all available winget updates;
+REM - Install all available winget updates;
 winget upgrade --all
 
-REM Start the Windows Update service;
+REM - Start the Windows Update service;
 net start wuauserv
 
-REM Open the Windows Update GUI;
+REM - Open the Windows Update GUI;
 control update
 
-REM Install Dell SupportAssist updates;
+REM - Install Dell SupportAssist updates;
+REM - NOT SURE ABOUT THIS: DellSupportAssist.exe
