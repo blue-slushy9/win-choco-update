8be02d31334a9bbb8f0ecea2b720118b5fba65ea
blue-slushy9 <ra.cruz971@gmail.com>
2024-02-03 19:41:31 -0800

    made a few improvements to the batch script

--- win_choco_update.bat
+++ win_choco_update.bat
@@ -1,25 +1,28 @@
 REM - This is a batch script that installs all available Chocolatey and winget updates,
 REM  - then it starts the Windows Update service and opens up the GUI for it;
-REM - PLEASE NOTE YOU MUST RUN CMD AS ADMINISTRATOR FOR CHOCO UPDATES, AND AS USER FOR WINGET UPDATES!
+REM - PLEASE NOTE YOU MUST RUN CMD AS ADMINISTRATOR FOR THIS SCRIPT TO WORK!
 
-REM - Install all available Chocolatey updates;
+:: '::' is the other comment notation in batch scripts,
+:: unlike REM it will not print the comment to the terminal;
+
+:: Install all available Chocolatey updates;
 choco upgrade all
 
-REM - THIS COMMAND MUST BE RUN IN USER TERMINAL, NOT ADMIN;
-REM - Install all available winget updates;
-REM - the password is that of the Microsoft account for blue_slushy9@protonmail.com;
+:: THIS COMMAND MUST BE RUN IN USER TERMINAL, NOT ADMIN;
+:: Install all available winget updates;
+:: the password is that of the Microsoft account 
+:: for blue_slushy9@protonmail.com;
 runas /user:racru "winget upgrade --all --force"
 
-REM - Start the Windows Update service;
+:: Start the Windows Update service;
 net start wuauserv
 
-REM - Open the Windows Update GUI;
+:: Open the Windows Update GUI;
 control update
 
-REM - Install Dell SupportAssist updates;
-REM - NOT SURE ABOUT THIS: DellSupportAssist.exe
+:: Install Dell SupportAssist updates;
+:: NOT SURE ABOUT THIS: DellSupportAssist.exe
 
-REM - '::' is the other comment notation in batch scripts;
 :: NOTES
 :: The package expressvpn wants to run 'chocolateyInstall.ps1'.
 :: Note: If you don't run this script, the installation will fail.
